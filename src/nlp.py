43c007285635ab89f0a4ca968570ae603e2c92b3
sandro <sbadame@gmail.com>
2015-11-08 22:46:44 -0500

    Break out nouns and sentiment'

--- src/nlp.py
+++ src/nlp.py
@@ -6,7 +6,9 @@ import json
 def parse(text):
     blob = TextBlob(text)
     sentiment = blob.sentiment
-    return (list(set(blob.noun_phrases + parse_nouns(blob))), {'polarity': sentiment.polarity, 'subjectivity': sentiment.subjectivity})
+    nouns = list(set(blob.noun_phrases + parse_nouns(blob)))
+    sentiment = {'polarity': sentiment.polarity, 'subjectivity': sentiment.subjectivity}
+    return (nouns, sentiment)
 
 def parse_nouns(blob):
   return [n
